57d478370a582486e77658cad721077ce14d0d24
Tomas Peterka <tomas.peterka@dronetag.cz>
2024-07-16 15:42:24 +0200

    fix: Fix signals on Windows

--- mavsniff/capture.py
+++ mavsniff/capture.py
@@ -3,6 +3,8 @@ import signal
 import struct
 import threading
 import time
+import os
+from typing import Any, Optional
 
 import pcapng
 import serial
@@ -18,13 +20,21 @@ class Capture:
 
     file: io.BufferedWriter
     device: mavutil.mavfile
-    writer: pcapng.FileWriter
+    writer: Optional[pcapng.FileWriter]
     done: bool
 
     def __init__(self, device: mavutil.mavfile, file: io.BufferedWriter):
         self.device = device
         self.file = file
         self.done = False
+        self.writer = None
+
+        self.received = 0
+        self.parse_errors = 0
+        self.empty_messages = 0
+        self.bad_messages = 0
+        self.other_messages = 0
+
         self.sbh = pcapng.blocks.SectionHeader(
             msgid=0,
             endianness="<",
@@ -49,55 +59,63 @@ class Capture:
                 },
             )
         )
-        signal.signal(signal.SIGINT, self.stop)
-        signal.signal(signal.SIGKILL, self.stop)
-        signal.signal(signal.SIGHUP, self.stop)
+
+
+    def stop(self, sig: int, frame: Optional[Any]) -> None:
+        logger.info(f"Termination event caught - quitting")
+        self.done = True
+
+
+    def report_stats(self):
+        while not self.done:
+            logger.info(
+                f"captured {self.received}, not-parsed: {self.other_messages}, "
+                f"empty: {self.empty_messages}/s, bad: {self.bad_messages}"
+            )
+            self.empty_messages = 0  # zero-out the empty messages counter or it overflows quickly
+            time.sleep(1.0)
 
 
     def run(self, limit: int=-1, limit_invalid_packets: int=-1) -> int:
         """Store Mavlink messages into a PCAPNG file"""
+        if self.writer is not None:
+            raise RuntimeError("Called run method twice")
+
+        signal.signal(signal.SIGINT, self.stop)
+        if os.name == "posix":
+            signal.signal(signal.SIGKILL, self.stop)
+            signal.signal(signal.SIGHUP, self.stop)
+
         self.writer = pcapng.FileWriter(self.file, self.sbh)
         self.done = False
-        received = 0
-        parse_errors = 0
-        empty_messages = 0
-        bad_messages = 0
-        other_messages = 0
 
-        def report_stats():
-            while not self.done:
-                logger.info(
-                    f"captured {received}, not-parsed: {other_messages}, "
-                    f"empty: {empty_messages}, bad: {bad_messages}"
-                )
-                time.sleep(1.0)
-
-        threading.Thread(target=report_stats).start()
+        threading.Thread(target=self.report_stats).start()
 
         while not self.done:
             try:
                 msg = self.device.recv_msg()
-                parse_errors = 0
+                self.parse_errors = 0
                 if msg is None:
-                    empty_messages += 1
+                    self.empty_messages += 1
                     continue
                 if msg.get_type() == "BAD_DATA":
-                    bad_messages += 1
+                    self.bad_messages += 1
                     continue
-                received += 1
-                self._write_packet(received, msg.pack(self.device.mav))
-                if limit > 0 and received >= limit:
+                self.received += 1
+                self._write_packet(self.received, msg.pack(self.device.mav))
+                if limit > 0 and self.received >= limit:
                     break
             except mavparse.MAVParseError:
-                parse_errors += 1
-                other_messages += 1
-                if limit_invalid_packets > 0 and parse_errors > limit_invalid_packets:
+                self.parse_errors += 1
+                self.other_messages += 1
+                if limit_invalid_packets > 0 and self.parse_errors > limit_invalid_packets:
                     raise RuntimeError("Too many invalid packets in a row")
                 continue
             except serial.SerialException:
                 logger.info("serial line closed")
                 break
-        return received
+        self.device.close()
+        return self.received
 
 
     def _write_packet(self, seq: int, data: bytes):
@@ -122,8 +140,3 @@ class Capture:
                 # },
             )
         )
-
-
-    def stop(self, *args, **kwargs):
-        logger.info(f"Termination event caught {args} - quitting")
-        self.done = True
